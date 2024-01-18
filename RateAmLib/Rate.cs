f8e86238182127652afcaea487fec20caae0ba30
Yeva <khazaryaneva@gmail.com>
2024-01-18 16:34:36 +0400

    test cmmet

--- RateAmLib/Rate.cs
+++ RateAmLib/Rate.cs
@@ -3,8 +3,9 @@
     public class Rate
     {
         public int Id { get; set; }
-        public string BuyRate { get; set; }
-        public string SellRate { get; set; }
+        public string Date {  get; set; }
+        public decimal BuyRate { get; set; }
+        public decimal SellRate { get; set; }
         public int BankId { get; set; }
         public Bank Bank { get; set; }
         public int CurrencyId {  get; set; }
